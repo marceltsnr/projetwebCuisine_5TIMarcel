9ca9f0c1dd074a8acf8296557128c420db1ef787
200046 - TASNIER MARCEL <200046@site.asty-moulin.be>
2026-03-18 21:48:29 +0100

    18-03-2026

--- Views/base.php
+++ Views/base.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;800&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../Assets/Css/base.css">
-    <link rel="stylesheet" href="../Assets/css/animation.css">
-    <link rel="stylesheet" href="../Assets/Css/flex.css">
-    <link rel="stylesheet" href="../Assets/css/form.css">
     <link rel="shortcut icon" href="../Assets/Images/logo.png" type="image/x-icon">
     <title><?= $title ?></title>
 </head>
